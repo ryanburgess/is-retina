55470dee86bdb74be6a9d1ecaa90207e5ffe7cce
Ryan <rburgess@evernote.com>
2015-01-23 13:24:36 -0800

    add project files

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# is-retina
+Is Retina
+=============
 JavaScript module to check if the device is retina or not.
+
+## Release History
+* 1.0.0: Initial release.
+ 
+## Contributing
+ 
+1. Fork it
+2. Create your feature branch (`git checkout -b my-new-feature`)
+3. Commit your changes (`git commit -am "Add some feature"`)
+4. Push to the branch (`git push origin my-new-feature`)
+5. Create new Pull Request
---
+++ index.js
@@ -0,0 +1,13 @@
+// isRetina
+//
+// Use: Determines whether or not the given device is retina
+//
+// Returns: Boolean
+// ------------------------------------------------------------
+
+module.exports = function isRetina(){
+  if (window.devicePixelRatio > 1 || window.matchMedia && window.matchMedia('(-webkit-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (min-device-pixel-ratio: 1.5), (min-resolution: 144dpi), (min-resolution: 1.5dppx)').matches) {
+    return true;
+  }
+  return false;
+};
