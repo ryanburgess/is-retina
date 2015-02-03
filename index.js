fb49fa6a39998cb1566a231bbf8a86f3c1dc312b
Ryan <rburgess@evernote.com>
2015-02-02 20:43:25 -0800

    add 'use strict'

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 // ------------------------------------------------------------
 
 module.exports = function retinaCheck(){
+  'use strict';
   if (window.devicePixelRatio > 1 || window.matchMedia && window.matchMedia('(-webkit-min-device-pixel-ratio: 1.5), (min--moz-device-pixel-ratio: 1.5), (-o-min-device-pixel-ratio: 3/2), (min-device-pixel-ratio: 1.5), (min-resolution: 144dpi), (min-resolution: 1.5dppx)').matches) {
     return true;
   }
